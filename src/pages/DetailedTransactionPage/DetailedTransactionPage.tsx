ce2a7c05623d1cb2088479aaae8e394ff3a8b7de
Anton Zhuravlev <abramoff.job@gmail.com>
2025-12-07 13:07:33 +0300

    fix

--- src/pages/DetailedTransactionPage/DetailedTransactionPage.tsx
+++ src/pages/DetailedTransactionPage/DetailedTransactionPage.tsx
@@ -1,39 +1,22 @@
-import { IconButton } from "@shared/ui";
-import cls from "./DetailedTransactionPage.module.scss"
-import { ArrowBackIcon } from "@shared/ui/icons";
-import { useNavigate, useParams } from "react-router-dom";
-import { formatDateToRu } from "@shared/lib";
-import { ExpenseDetailed } from "@entities/expense";
-import type { Operation } from "@entities/expense/interface/operation.interface";
-import { useLazyGetOperationQuery } from "@entities/expense/api";
-import { useEffect } from "react";
-
-const data: Operation = {
-    category: "Food",
-    refNo: "aeboba",
-    sum: 1000,
-    transactionDate: new Date(),
-    purchases: [
-        {
-            count: 10,
-            name: "Хрен",
-            price: 100
-        }
-    ]
-}
+import { IconButton } from '@shared/ui';
+import cls from './DetailedTransactionPage.module.scss';
+import { ArrowBackIcon } from '@shared/ui/icons';
+import { useNavigate, useParams } from 'react-router-dom';
+import { formatDateToRu } from '@shared/lib';
+import { ExpenseDetailed } from '@entities/expense';
+import { useLazyGetOperationQuery } from '@entities/expense/api';
+import { useEffect } from 'react';
 
 export const DetailedTransactionPage = () => {
     const { id } = useParams<{ id: string }>();
     const [trigger, { data }] = useLazyGetOperationQuery();
     const navigate = useNavigate();
-    
 
     useEffect(() => {
         if (id) {
-            trigger({id: +id});
+            trigger({ id: +id });
         }
     }, [id, trigger]);
-
 
     return (
         <div className={cls.root}>
@@ -48,11 +31,13 @@ export const DetailedTransactionPage = () => {
                             onClick={() => navigate(-1)}
                         />
                     </div>
-                    {data &&                
-                        <h1 className={cls.title}>{formatDateToRu(data.transactionDate.toString())}</h1>
-                    }
+                    {data && (
+                        <h1 className={cls.title}>
+                            {formatDateToRu(data.transactionDate.toString())}
+                        </h1>
+                    )}
                 </div>
-                {data && <ExpenseDetailed transaction={data}/>}
+                {data && <ExpenseDetailed transaction={data} />}
             </div>
         </div>
     );
